368eec11c3784e6e40c28ba62943dbfcdf1ab22d
Ginthozan Varnakulasingam <ginthozanv@gmail.com>
2022-05-01 22:25:26 +0530

    fix eslint

--- components/Header/account.js
+++ components/Header/account.js
@@ -1,7 +1,7 @@
 import { UserIcon } from "@heroicons/react/solid";
 import { useState } from "react";
-import Login from "../Views/Auth/Login";
-import Register from "../Views/Auth/register";
+import Login from '../Views/Auth/login'
+import Register from '../Views/Auth/register'
 
 const Account = () => {
   const [isLogin, setIsLogin] = useState(false);
@@ -12,7 +12,7 @@ const Account = () => {
         <UserIcon className="w-5 h-5 md:w-7 md:h-7 text-gray-500 cursor-pointer" />
       </button>
       {isLogin && (
-        <Login setIsLogin={setIsLogin} setIsRegister={setIsRegister} />
+        <Login  setIsLogin={setIsLogin} setIsRegister={setIsRegister} />
       )}
       {isRegister && (
         <Register setIsRegister={setIsRegister} setIsLogin={setIsLogin} />
